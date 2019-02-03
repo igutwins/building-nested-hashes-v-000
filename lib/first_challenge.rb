c58c126bf17a71c1f2efb7a6607ea20ddbdf7cd9
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-03 21:12:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,34 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     "Montagues" => {
+       "Patriarch" => {
+          name: "Lord Montague",
+          age: 53,
+       },
+       "Matriarch" => {
+         name: "Lady Montague"
+         age: 54
+       },
+       "Hero" => {
+         name: "Romeo",
+         age: 15,
+         status: "Alive"
+       },
+       "Hero's Friends" => {
+         "Friend I" => {
+           name: "Benvolio",
+           age: 17,
+           attitude: "worried"
+         },
+         "Friend II" => {
+           name: "Mercutio",
+           age: 18,
+           attitude: "hot-headed"
+         }
+       }
+     }
+     "Capulets" => {
+       
+     }
+   }
 end
