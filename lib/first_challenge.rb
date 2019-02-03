9d52499c335c105915a4c1ca09f6d7a5173c2528
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-03 21:17:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,31 +1,29 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       "Patriarch" => {
-          name: "Lord Montague",
-          age: 53,
+       :patriarch => {
+          :name => "Lord Montague",
+          :age => 53,
        },
-       "Matriarch" => {
-         name: "Lady Montague"
-         age: 54
+       :matriarch => {
+         :name =>"Lady Montague"
+         :age => 54
        },
-       "Hero" => {
-         name: "Romeo",
-         age: 15,
-         status: "Alive"
+       :hero => {
+         :name => "Romeo",
+         :age => 15,
+         :status => "Alive"
        },
-       "Hero's Friends" => {
-         "Friend I" => {
-           name: "Benvolio",
-           age: 17,
-           attitude: "worried"
+       :hero_friends => [
+         { :name => "Benvolio",
+           :age => 17,
+           :attitude => "worried"
          },
-         "Friend II" => {
-           name: "Mercutio",
-           age: 18,
-           attitude: "hot-headed"
+         { :name => "Mercutio",
+           :age => 18,
+           :attitude => "hot-headed"
          }
-       }
+       ]
      }
      :capulet => {
 
