19fefd3984e93376bb918260bd784e14dc4fcc2d
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-03 21:20:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -39,6 +39,16 @@ def first_challenge
          :age => 15,
          :status => "Alive"
        }
+       :heroine_friends => [
+         { :name => "Steven",
+           :age => 30
+           :attitude => "confused"
+         },
+         { :name => "Nurse",
+           :age => 44,
+           :attitude => "worried"
+         }
+       ]
 
      }
    }
